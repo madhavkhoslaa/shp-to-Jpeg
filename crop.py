8d6df396291ca1ff3ffe98fd66ec68227b9a8d97
Madhav Khosla <28945483+madhavkhoslaa@users.noreply.github.com>
2019-08-17 14:51:42 +0530

    Update crop.py

--- crop.py
+++ crop.py
@@ -1,31 +1,40 @@
 import shapefile
 import numpy as np
-import sys
 from PIL import Image
 from shapely.geometry import Point,Polygon
-import os
+import os, glob
+import shutil
+from tqdm import tqdm
 
-name = sys.argv[1]
-os.chdir(name[:-1])
-sf = shapefile.Reader(name+'.shp')
-for poly_ in range(len(sf.__geo_interface__)): 
-    poly =list(sf.__geo_interface__['features'][poly_]['geometry']['coordinates'][0])
-    poly = [(-v,k) for (k,v) in poly]
-    im = Image.open(name+'.png').convert("RGBA")
-    pixels = np.array(im)
-    im_copy = np.array(im)
-    region=Polygon(poly)
-    for index, pixel in np.ndenumerate(pixels):
-    	row,col,channel = index
-    	if channel != 0:
-    		continue
-    	point = Point(row,col)
-    	if not region.contains(point):
-    		im_copy[(row,col,0)]=255
-    		im_copy[(row,col,1)]=255
-    		im_copy[(row,col,2)]=255
-    		im_copy[(row,col,3)]=0
-    # back to Image from numpy
-    newIm = Image.fromarray(im_copy, "RGBA")
-    nm = name+str(poly_)+'_cropped.png'
-    newIm.save(nm)
+
+for i in tqdm(range(0, 500)):
+    try:
+        name = "/Users/madhav/Desktop/OUTPUT/Output/Defence Colony/{}".format(i)
+        shape_file= glob.glob(name+"/*.shp")[0]
+        image_name= glob.glob(name+"/*.png")[0]
+        sf = shapefile.Reader(shape_file)
+        for poly_ in range(len(sf.__geo_interface__)):
+            poly =list(sf.__geo_interface__['features'][poly_]['geometry']['coordinates'][0])
+            poly = [(-v,k) for (k,v) in poly]
+            im = Image.open(image_name).convert("RGBA")
+            pixels = np.array(im)
+            im_copy = np.array(im)
+            region=Polygon(poly)
+            for index, pixel in np.ndenumerate(pixels):
+            	row,col,channel = index
+            	if channel != 0:
+            		continue
+            	point = Point(row,col)
+            	if not region.contains(point):
+            		im_copy[(row,col,0)]=255
+            		im_copy[(row,col,1)]=255
+            		im_copy[(row,col,2)]=255
+            		im_copy[(row,col,3)]=0
+            # back to Image from numpy
+            os.chdir(name)
+            newIm = Image.fromarray(im_copy, "RGBA")
+            nm = name+"_"+str(poly_)+'_cropped.png'
+            newIm.save(nm)
+            shutil.move(nm, name)
+    except :
+        pass
